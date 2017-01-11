babb8f97efeafd2742856fa46aea714af5a85e53
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2017-01-12 02:16:22 +0800

    confirming tests for landmark window

--- test/landmark_window_of_int_tests.erl
+++ test/landmark_window_of_int_tests.erl
@@ -14,12 +14,22 @@ server_test() ->
     ?assertEqual(ok, Terminated),
     ok.
 
+expect_count(N, Pid) when is_integer(N) andalso is_pid(Pid) ->
+    Count = gen_server:call(Pid, #query{ term= count }),
+    Query = gen_server:call(Pid, #query{ term= inspect }),
+    ?assertEqual(N, Count),
+    ?assertEqual(N, Query#state.next - 1),
+    ?assertEqual(N, dict:size(Query#state.elements)),
+    ok.
+
 windowing_test() ->
     {ok, Pid} = landmark_window_of_int:start_link(),
     gen_server:call(Pid, #data{ type= int, value= 100 }),
-    gen_server:cast(Pid, #data{ type= int, value= 99 }),
-    Query = gen_server:call(Pid, #query{ term= inspect }),
-    ?assertEqual(Query#state.next - 1, dict:size(Query#state.elements)),
+    expect_count(1, Pid),
+    gen_server:call(Pid, #data{ type= int, value= 99 }),
+    expect_count(2, Pid),
+    gen_server:call(Pid, #data{ type= int, value= 98 }),
+    expect_count(3, Pid),
     ok = gen_server:stop(Pid),
     ok.
      
