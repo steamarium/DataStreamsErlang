98444d592f27a0179049ce04fc4a74ef856f976a
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2017-01-12 00:05:03 +0800

    first commit

---
+++ test/sliding_window_of_int_tests.erl
@@ -0,0 +1,10 @@
+-module(sliding_window_of_int_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+searver_test() ->
+    {ok, Pid} = sliding_window_of_int:start_link(),
+    SyncCall = gen_server:call(Pid, any),
+    ?assertEqual(ok, SyncCall),
+    Terminated = gen_server:stop(Pid),
+    ?assertEqual(ok, Terminated),
+    ok.
