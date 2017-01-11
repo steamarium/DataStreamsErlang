09dedc2f85bc39a6c98ab68959dda8cfbeba5ca9
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2017-01-12 02:07:26 +0800

    implementing sliding window

--- src/sliding_window_of_int.erl
+++ src/sliding_window_of_int.erl
@@ -2,11 +2,22 @@
 -behaviour(gen_server).
 -define(SERVER, ?MODULE).
 
+%% In the book "Knowledage Discovery from Data Streams" the sliding window approach was introduced in the section 2.2.5,
+%% Fig. 2.4. This program implement a sliding window with elements of type integer.
+
+-include("../include/data.hrl").
+-include("../include/query.hrl").
+-record(state, { next = 1 :: integer(),
+		 size :: integer(),
+	         elements = dict:new() :: dict:dict() %% a dictionary of {Int, #data{}}
+	       }).
+
+
 %% ------------------------------------------------------------------
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/0]).
+-export([start_link/1]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
@@ -19,19 +30,31 @@
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
-start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+start_link(WindowSize) ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [WindowSize], []).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
-init(Args) ->
-    {ok, Args}.
+init([WindowSize]) ->
+    {ok, #state{ size= WindowSize }}.
 
-handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
+handle_call(#data{ type= int, value= Value }= Data, _from, State) when is_integer(Value) ->
+    State1 = append(Data, State),
+    {reply, ok, State1};
+handle_call(#query{ term= count }, _from, #state{ next= Next, size= WindowSize }= State) ->
+    {reply, if Next - 1 < WindowSize -> Next - 1;
+	       true -> WindowSize end,
+     State};
+handle_call(#query{ term= inspect }, _from, State) ->
+    {reply, State, State};
+handle_call(Request, _From, State) ->
+    {reply, {drop, Request}, State}.
 
+handle_cast(#data{ type= int, value= Value }= Data, State) when is_integer(Value) ->
+    State1 = append(Data, State),
+    {noreply, State1};
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
@@ -48,3 +71,9 @@ code_change(_OldVsn, State, _Extra) ->
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
 
+append(Data, #state{ next= Next, size= WindowSize }= State) ->
+    ToDrop = Next - WindowSize,
+    State#state{ next = State#state.next + 1,
+		 elements = dict:append(State#state.next, Data,
+					if ToDrop =< 0 -> State#state.elements;
+					   true -> dict:erase(ToDrop, State#state.elements) end) }.
--- test/sliding_window_of_int_tests.erl
+++ test/sliding_window_of_int_tests.erl
@@ -1,10 +1,45 @@
 -module(sliding_window_of_int_tests).
 -include_lib("eunit/include/eunit.hrl").
+-include("../include/data.hrl").
+-include("../include/query.hrl").
+-record(state, { next = 1 :: integer(),
+		 size :: integer(),
+	         elements = dict:new() :: dict:dict() %% a dictionary of {Int, #data{}}
+	       }).
+-define(module, sliding_window_of_int).
 
 searver_test() ->
-    {ok, Pid} = sliding_window_of_int:start_link(),
+    {ok, Pid} = ?module:start_link(0),
     SyncCall = gen_server:call(Pid, any),
-    ?assertEqual(ok, SyncCall),
+    ?assertEqual({drop, any}, SyncCall),
     Terminated = gen_server:stop(Pid),
     ?assertEqual(ok, Terminated),
+    ok.
+
+count(#state{}= Query) ->
+    if Query#state.next - 1 < Query#state.size ->
+	    Query#state.next - 1;
+       true ->
+	    Query#state.size
+    end.
+
+check_count(Pid) when is_pid(Pid) ->
+    Count = gen_server:call(Pid, #query{ term= count }),
+    Query = gen_server:call(Pid, #query{ term= inspect }),
+    ?assertEqual(Count, if Query#state.next - 1 < Query#state.size -> Query#state.next - 1; 
+			   true -> Query#state.size end ),
+    ?assertEqual(Count, dict:size(Query#state.elements)),
+    ok.
+
+windowing_test() ->
+    {ok, Pid} = ?module:start_link(2),
+    gen_server:call(Pid, #data{ type= int, value= 100 }),
+    check_count(Pid),
+    gen_server:call(Pid, #data{ type= int, value= 99 }),
+    check_count(Pid),
+    gen_server:call(Pid, #data{ type= int, value= 98 }),
+    check_count(Pid),
+    gen_server:call(Pid, #data{ type= int, value= 97 }),
+    check_count(Pid),
+    ok = gen_server:stop(Pid),
     ok.
